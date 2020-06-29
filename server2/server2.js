535d08bf0990991dc44d07eb229ac22f40686b83
gabriel <gabriel.ramirez.lemus@gmail.com>
2020-06-29 03:55:30 -0400

    se agregó autenticacion para api de usuarios

--- server2/server2.js
+++ server2/server2.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
